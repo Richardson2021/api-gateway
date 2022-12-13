45698b560ce67f033baef933c3ca31c867a97a41
Richardson2021 <96339724+Richardson2021@users.noreply.github.com>
2022-12-13 10:40:18 +0300

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import logo from './logo.svg';
+import './App.css';
+
+<style> @import url('https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,100;0,300;0,400;0,500;0,700;0,900;1,100;1,300;1,400;1,500;1,700;1,900&display=swap'); </style>
+
+function App() {
+  return (
+    <header className="navbar">
+      <button className="button">WOMEN</button>
+      <button className="button">MEN</button>
+      <button className="button">KIDS</button>
+      </header>
+  );
+}
+
+export default App;
